1b5d830a8759769fd2b4251bfbb6de444848458e
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-05-05 11:27:59 -0500

    Add user and ticket association

--- models/user.js
+++ models/user.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Ticket, {as: 'ticket', foreignKey: 'userId'})
     }
   }
   User.init({
