cfdf48f7940edfcd035b9cb3ea6257fa72f47e36
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-05-05 10:44:20 -0500

    Setting up system files

---
+++ routes/AppRouter.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const Router = express.Router()
+// const UserRouter = require('./UserRouter')
+// const TrailRouter = require('./TrailRouter')
+// const StateRouter = require('./StateRouter')
+// const PostRouter = require('./PostRouter')
+// const CommentRouter = require('./CommentRouter')
+
+// Router.use('/user', UserRouter)
+// Router.use('/trail', TrailRouter)
+// Router.use('/state', StateRouter)
+// Router.use('/comment', CommentRouter)
+// Router.use('/post', PostRouter)
+
+module.exports = Router
