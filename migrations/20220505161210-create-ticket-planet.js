435388ef0766ebaa1ee13c360bf0133007e8b4cf
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-05-05 11:12:34 -0500

    create TicketPlanet model

---
+++ migrations/20220505161210-create-ticket-planet.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('TicketPlanets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ticketId: {
+        type: Sequelize.INTEGER
+      },
+      planetId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('TicketPlanets');
+  }
+};
